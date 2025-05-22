4aa66aadbdeaa33c59d6fb8ac2d493d23243af18
Saksham Soni <sonisaksham20208@gmail.com>
2025-05-22 20:55:10 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pickle
+from PIL import Image
+
+
+
+
+def main():
+    st.title("Maatricare")
+    st.subheader(" _Personalized_ _AI-Based_ :green[_Nutrition_] _&_ :green[_Maternal_ _Care_] ")
+    st.markdown("<hr style='border:1px solid gray'>", unsafe_allow_html=True)
+
+    
+    option = st.selectbox(
+    "Choose a service",
+    ("Check Symptoms (AI Assistant)", "Detect Signs of Malnutrition", "Personalized Diet Plan", "Tips for Newborn Care"),
+    )
+
+
+
+ 
+
+if __name__=='__main__':
+    main()
